3d57a5ce17419f39c720f9d09c9dbd457aa7efce
Troy Neubauer <troyneubauer@gmail.com>
2020-06-28 10:44:38 -0400

    Refactored example code

---
+++ src/main/java/com/troy/tco/Constants.java
@@ -0,0 +1,7 @@
+package com.troy.tco;
+
+public class Constants {
+    public static final String MODID = "TCO";
+    public static final String NAME = "TCO";
+    public static final String VERSION = "v0.1.0";
+}
--- src/main/java/com/troy/tco/ExampleMod.java
+++ src/main/java/com/troy/tco/ExampleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.troy.tco;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,13 +7,9 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
+@Mod(modid = Constants.MODID, name = Constants.NAME, version = Constants.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
-
     private static Logger logger;
 
     @EventHandler
@@ -25,7 +21,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
         logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
